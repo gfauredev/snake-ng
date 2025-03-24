5a966229e03b6be64a484b06a37abf88463f564b
Guilhem Fauré <pro@gfaure.eu>
2025-03-24 21:42:14 +0100

    feat: logging

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+// #include <stdint.h>
 
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
@@ -42,6 +43,7 @@ SDL_AppResult SDL_AppIterate(void* appstate)
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer); // Set the whole window black
 
+    SDL_Log("Displaying various flavors of hellos");
     SDL_SetRenderScale(renderer, 2.0f, 2.0f); // Double text size
     // Text white, then blue
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);

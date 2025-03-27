d84ff6534b3922a4b36b49b09a2964f274f98af1
Guilhem Fauré <pro@gfaure.eu>
2025-03-27 14:25:26 +0100

    ci: add arm nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   # inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
         # [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f:
         nixpkgs.lib.genAttrs supportedSystems
